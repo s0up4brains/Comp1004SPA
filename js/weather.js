410c0ac2c03d81430ecdb3f71253163be5c305f3
s0up4brains <liamrjames@icloud.com>
2024-03-29 21:25:39 +0000

    API feature Strip Back and Redesign
    Decided to switch API requests to provide more relevant information. CSS redesign is underway

--- js/weather.js
+++ js/weather.js
@@ -1,95 +1,113 @@
 //Define API KEY
-apiKey = 'f6cb48ac149fb100cad23dae780eeb9b';
+apiKey = '01094cef1967b9113eadf4f6efd0139d';
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+var x = document.getElementById("Geolocation");
 function defaultWeather(){ //Define Function
-    var city = "london" //Get Location from user input in search bar
+    //Recieve user location from Geolocation
+    if (navigator.geolocation) { 
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+        //Geo Location Error Handling
+        function showError(error) {
+            switch(error.code) {
+                case error.PERMISSION_DENIED:
+                x.innerHTML = "Please allow Location Access to see the stargazing conditions in your area."
+                break;
+                case error.POSITION_UNAVAILABLE:
+                x.innerHTML = "Location information is unavailable."
+                break;
+            }}
 
-//Current Weather
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + apiKey) //Fetch API from OpenWeathermap.org
-    .then((response) => response.json())
-    .then((data) => {
-        //Recieve Data From the JSON file in the API and assign to variables
-        const {description, icon} = data.weather[0];
-        const {temp, temp_min, temp_max, pressure, humidity} = data.main;
-        const {speed} = data.wind;
-        const {all} = data.clouds;
-        const {country} = data.sys;
+            //Display User Position
+            function showPosition(position) {
+                //Get Geolocation
+                navigator.geolocation.getCurrentPosition(position => {
+                    const { latitude, longitude } = position.coords;
+                    fetch("http://api.openweathermap.org/geo/1.0/reverse?lat=" + latitude + "&lon=" + longitude + "&limit=2&appid=" + apiKey) //Fetch API from OpenWeathermap.org
+                    .then((response) => response.json())
+                    .then((data) => {
+                        
+                        const {name,country} = data[0];
+                        var city = document.querySelector(".city").innerText = (`${name}`) + ", " + (`${country}`);
+                    
+                        //Tonight Daily Summary
+                        fetch("http://api.openweathermap.org/data/2.5/forecast/daily?q=" + city + "&units=metric&cnt=3&appid=" + apiKey) //Fetch API from OpenWeathermap.org
+                        .then((response) => response.json())
+                        .then((data) => {
+                            //Recieve Data From the JSON file in the API and assign to variables
+                            // const {sunset, clouds, rain,pop,speed} = data.list[0];
+                            
+                            const {description} = data.list[0].weather[0];
+                            const {min, max, eve, night} = data.list[0].temp;
+                            const {dt, sunset, clouds, pop, rain} = data.list[0];
+                            
+                            const unixTimestamp = dt; 
+                            const myDate = new Date(unixTimestamp * 1000); 
+                            
+                            
+                            
+                            //Writes variables to HTML Class
+                            document.querySelector(".myDate").innerText = (`${myDate.toDateString()}`);
+                            document.querySelector(".description").innerText = (`${description}`);
+                            //Temps
+                            document.querySelector(".min").innerText = (`Low: ${min} °C`);
+                            document.querySelector(".max").innerText = (`High: ${max} °C`);
+                            // document.querySelector(".eve").innerText = (`18:00 ${eve}`);
+                            // document.querySelector(".night").innerText = (`00:00: ${night} °C`);
+                            
+                            document.querySelector(".sunset").innerText = (`${sunset}`);
+                            document.querySelector(".clouds").innerText = (`${clouds}`);
+                            document.querySelector(".pop").innerText = (`${pop}`);
+                            document.querySelector(".rain").innerText = (`${rain}`);
+                            
+  
+                        
+                        });
+                        
+                        //Tonight Forecast
 
-
-        //Writes variables to HTML Class
-        document.querySelector(".writeIcon").src="https://openweathermap.org/img/wn/"+ icon + ".png",
-        document.querySelector(".city").innerText = (`${city.toUpperCase()}, ${country}`),
-        document.querySelector(".description").innerText = (`${description}`),
-        document.querySelector(".temp").innerText = (`${temp}°C`),
-        document.querySelector(".temp_range").innerText = (`High of ${temp_max} °C  and low of ${temp_min}°C`)
-        document.querySelector(".speed").innerText = (`Wind Speed: ${speed} (m/s)`),
-        document.querySelector(".clouds").innerText = (`Cloud Coverage: ${all} %`)
-
-        ; 
-    });
-
-    //7 day forecast
-    fetch("https://api.openweathermap.org/data/2.5/forecast/daily?q=" + city + "&cnt=14&units=metric&appid=" + apiKey) //Fetch API from OpenWeathermap.org
-        .then((response) => response.json())
-        .then((data) => {
-            for (i=0;i<7;i++){
-
-                const {max, min} = data.list[i].temp;
-                const {icon} = data.list[i].weather[0];
-                    document.querySelector(".forecastIcon"+(i+1)).src="https://openweathermap.org/img/wn/"+ icon + ".png",
-                    document.querySelector(".max" +(i+1)).innerText = (`${max}°C`),
-                    document.querySelector(".min" +(i+1)).innerText = (`${min}°C`);
-
-    
+                        
+                    });
+                });
             }
-    });
-
-}
-
-function fetchWeather(){ //Define Function
-    var city = document.getElementById("search-input").value; //Get Location from user input in search bar
-
-
-
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + apiKey) //Fetch API from OpenWeathermap.org
-    .then((response) => response.json())
-    .then((data) => {
-        //Recieve Data From the JSON file in the API and assign to variables
-        const {description, icon} = data.weather[0];
-        const {temp, temp_min, temp_max, pressure, humidity} = data.main;
-        const {speed} = data.wind;
-        const {all} = data.clouds;
-        const {country} = data.sys;
-
-
-        //Writes variables to HTML Class
-        document.querySelector(".writeIcon").src="https://openweathermap.org/img/wn/"+ icon + ".png",
-        document.querySelector(".city").innerText = (`${city.toUpperCase()}, ${country}`),
-        document.querySelector(".description").innerText = (`${description}`),
-        document.querySelector(".temp").innerText = (`${temp}°C`),
-        document.querySelector(".temp_range").innerText = (`High of ${temp_max} °C  and low of ${temp_min}°C`)
-        document.querySelector(".speed").innerText = (`Wind Speed: ${speed} (m/s)`),
-        document.querySelector(".clouds").innerText = (`Cloud Coverage: ${all} %`)
-
-        ; 
-    });
-
-    //14 day forecast
-    fetch("https://api.openweathermap.org/data/2.5/forecast/daily?q=" + city + "&cnt=14&units=metric&appid=" + apiKey) //Fetch API from OpenWeathermap.org
-        .then((response) => response.json())
-        .then((data) => {
-            for (i=0;i<14;i++){
-
-                const {max, min} = data.list[i].temp;
-                const {icon} = data.list[i].weather[0];
-                    document.querySelector(".forecastIcon"+(i+1)).src="https://openweathermap.org/img/wn/"+ icon + ".png",
-                    document.querySelector(".max" +(i+1)).innerText = (`${max}°C`),
-                    document.querySelector(".min" +(i+1)).innerText = (`${min}°C`);
-
+        }
     
-            }
-    });
-
-}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+
+
+
+// //Search Weather
+// function fetchWeather(){ //Define Function
+//     var city = document.getElementById("search-input").value; //Get Location from user input in search bar
+
+
+//     // CURRENT WEATHER FROM SEARCH
+//     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + apiKey) //Fetch API from OpenWeathermap.org
+//     .then((response) => response.json())
+//     .then((data) => {
+//         //Recieve Data From the JSON file in the API and assign to variables
+//         const {description, icon} = data.weather[0];
+//         const {temp, temp_min, temp_max, pressure, humidity} = data.main;
+//         const {speed} = data.wind;
+//         const {all} = data.clouds;
+//         const {country} = data.sys;
+
+
+//         //Writes variables to HTML Class
+//         document.querySelector(".writeIcon").src="https://openweathermap.org/img/wn/"+ icon + ".png",
+//         document.querySelector(".city").innerText = (`${city.toUpperCase()}, ${country}`),
+//         document.querySelector(".description").innerText = (`${description}`),
+//         document.querySelector(".temp").innerText = (`${temp}°C`),
+//         document.querySelector(".temp_range").innerText = (`High of ${temp_max} °C  and low of ${temp_min}°C`)
+//         document.querySelector(".speed").innerText = (`Wind Speed: ${speed} (m/s)`),
+//         document.querySelector(".clouds").innerText = (`Cloud Coverage: ${all} %`)
+
+//         ; 
+//     });
+
+// }
